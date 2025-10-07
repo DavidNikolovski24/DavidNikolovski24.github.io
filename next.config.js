a48e69d02e977f82625a207f32488b897261284e
unknown <david.nikolovski.00@gmail.com>
2025-10-07 12:37:18 +0200

    added anotherr next config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  basePath: '/DavidNikolovski24.github.io',
+  output: 'export', // Next.js 14 static HTML
+  images: { unoptimized: true }, // needed for GitHub Pages
+  basePath: '/DavidNikolovski24.github.io', // your repo name
   assetPrefix: '/DavidNikolovski24.github.io/',
+  trailingSlash: true, // ensures paths end with /
 };
 
 module.exports = nextConfig;
